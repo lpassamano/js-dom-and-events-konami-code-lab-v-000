8606c473f5bb9dd6d90a22ed0dbaffe6b667fd1d
lpassamano <leighayna.josele@gmail.com>
2018-01-02 18:30:05 -0500

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,25 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
   // Write your JavaScript code inside the init() function
+  document.body.addEventListener('keydown', onKeyDown);
+}
 
+let index = 0;
+
+function onKeyDown(e) {
+  const key = parseInt(e.which || e.detail);
+  console.log(key);
+
+  if (key === code[index]) {
+    console.log(key);
+    index++;
+    console.log(index);
+    if (index === code.length) {
+      alert("You've cracked the code!");
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
 }
